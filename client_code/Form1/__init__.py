fa102231741e46f302e1f81d093295f21f40842a
ccirone2@gmail.com <ccirone2@gmail.com>
2024-12-26 21:45:36 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,26 +10,20 @@ class Form1(Form1Template):
 
     # Any code you write here will run before the form opens.
 
-  def switch_1_change(self, **event_args):
-    """This method is called when the state of the component is changed."""
-    if self.switch_1.selected:
-      anvil.server.call("switch_1_on")
-      return
+  def button_1_on_click(self, **event_args):
+    anvil.server.call("switch_1_on")
+
+  def button_1_off_click(self, **event_args):
     anvil.server.call("switch_1_off")
 
-  def switch_2_change(self, **event_args):
-    """This method is called when the state of the component is changed."""
-    if self.switch_2.selected:
-      anvil.server.call("switch_2_on")
-      return
+  def button_2_on_click(self, **event_args):
+    anvil.server.call("switch_2_on")
+
+  def button_2_off_click(self, **event_args):
     anvil.server.call("switch_2_off")
 
-  def switch_3_change(self, **event_args):
-    """This method is called when the state of the component is changed."""
-    if self.switch_3.selected:
-      anvil.server.call("switch_3_on")
-      return
+  def button_3_on_click(self, **event_args):
+    anvil.server.call("switch_3_on")
+
+  def button_3_off_click(self, **event_args):
     anvil.server.call("switch_3_off")
-
-
-
