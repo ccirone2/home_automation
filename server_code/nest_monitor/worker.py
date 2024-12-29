e2443e225e846a2a81c37dd45600f37f6f73636a
Chris Cirone <ccirone2@gmail.com>
2024-12-28 23:18:53 -0600

    update

--- server_code/nest_monitor/worker.py
+++ server_code/nest_monitor/worker.py
@@ -1,25 +1,23 @@
 # nest_monitor/worker.py
 import anvil.server
 
-from nest_monitor import auth
-from nest_monitor import thermostat
-from nest_monitor import db_utils as database
-
 
 @anvil.server.callable
 def collect_temperature_data():
-    """Background task to collect temperature data every 10 minutes"""
-    while True:
-        try:
-            credentials = auth.setup_nest_credentials()
-            access_token = auth.get_access_token(credentials)
-            temperature = thermostat.get_temperature(access_token)
-            database.save_temperature_reading(temperature)
-            database.cleanup_old_readings()
+    from . import auth
+    from . import thermostat
+    from . import db_utils as database
 
-        except Exception as e:
-            print(f"Error collecting temperature data: {e}")
-            pass
+    try:
+        credentials = auth.setup_nest_credentials()
+        access_token = auth.get_access_token(credentials)
+        temperature = thermostat.get_temperature(access_token)
+        database.save_temperature_reading(temperature)
+        database.cleanup_old_readings()
+
+    except Exception as e:
+        print(f"Error collecting temperature data: {e}")
+        pass
 
 
 if __name__ == "__main__":
--- setup.py
+++
@@ -1,9 +0,0 @@
-# setup.py
-from setuptools import setup, find_namespace_packages
-
-setup(
-    name="home_automation",
-    version="0.1",
-    packages=find_namespace_packages(include=["server_code.*"]),
-    package_dir={"": "."},
-)
